16d0bc294fb75ddb184096badb1fad2eb8b3bb75
Harbin Ramo <hramo@my.centennialcollege.ca>
2019-11-04 16:22:35 -0500

    Updated SportsStore

---
+++ Models/FakeProductRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    /// <summary>
+    /// Dependency injection
+    /// </summary>
+    public class FakeProductRepository : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product>
+        {
+             new Product
+             {
+                 Name = "Football",
+                 Description = "Land",
+                 Price = 25,
+                 Category = "Ball Sports"
+             },
+             new Product
+             {
+                 Name = "Surf board",
+                 Description = "Water",
+                 Price = 179,
+                 Category = "Water Sports"
+             },
+             new Product
+             {
+                 Name = "Running shoes",
+                 Description = "Land",
+                 Price = 95,
+                 Category = "Running Sports"
+             }
+        }.AsQueryable<Product>();
+    }
+}
